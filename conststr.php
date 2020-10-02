005f870b5f1b7fe3867809d7d98dacc39e0f6676
xrkxrk <33193003+xrkxrk@users.noreply.github.com>
2020-10-02 11:27:13 +0800

    Update conststr.php

--- conststr.php
+++ conststr.php
@@ -12,8 +12,8 @@ $exts['zip'] = ['zip', 'rar', '7z', 'gz', 'tar'];
 
 $constStr = [
     'languages' => [
-        'en-us' => 'English',
         'zh-cn' => '简体中文',
+        'en-us' => 'English',
         'ja' => '日本語',
         'ko-kr' => '한국어',
         'fa' => 'فارسی',
